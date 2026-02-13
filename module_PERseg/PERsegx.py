018dfaafb00500d75ff7b188c865791a5f777337
abedul59 <70437442+abedul59@users.noreply.github.com>
2026-02-13 18:25:03 +0800

    Update table extraction logic in PERsegx.py
    Modify table extraction to retrieve all tables and print their count and content.

--- module_PERseg/PERsegx.py
+++ module_PERseg/PERsegx.py
@@ -411,7 +411,11 @@ def PERsegx(stock_id, month_id):
 #print(html.text) #以json格式呈現
 
         soup = BeautifulSoup(r.content, 'lxml')
-        table = soup.find_all('table')[0]  #原本是[2] 沒變
+        table = soup.find_all('table')
+        print(len(table))
+        print(table[0])
+        print(table[1])
+        
         dfs = pd.read_html(str(table))
         #print(soup)
         print(dfs)
